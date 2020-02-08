01d39b3c9915d8e6bd643a5b71b81df68cdf66af
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-02-07 21:47:36 -0800

    create most of the files and dependencies I need

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
