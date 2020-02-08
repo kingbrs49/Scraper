96f7a936f41693543cf746d1ffbf809e25b0c9f6
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-02-07 23:09:35 -0800

    added appropriate models

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
 
